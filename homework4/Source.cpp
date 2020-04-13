f1590d8d049742b8b325c55a6f5ad60c25d3097a
wonjoo <wonjoo0423@snu.ac.kr>
2020-04-13 12:36:08 +0900

    homework done

--- homework4/Source.cpp
+++ homework4/Source.cpp
@@ -24,10 +24,19 @@ int main()
 
 	int* output = new int[arrsize];
 	cout << "Array output: ";
-
+	
 	/***********************************
 	Implement the code here!
 	************************************/
+	for (int i = 0; i < arrsize; i++) {
+		int s = 0;
+		for (int j = 0; j < arrsize; j++) {
+			if (arr[i] > arr[j]) {
+				s++;
+			}
+		}
+		output[i] = s;
+	}
 
 	for (int i = 0; i < arrsize; i++) {
 		cout << output[i] << " ";
